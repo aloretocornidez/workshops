783a85d02fa3c7e08f4a4244e2a20ff5fd9713c1
Alan Manuel Loreto Cornidez <aloretocornidez@gmail.com>
2025-01-27 22:23:38 -0700

    create working build for opencv

---
+++ cmake-workshop/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[])
+{
+
+  // Print out hello world.
+  std::cout << "Hello World" << std::endl;
+
+  cv::Mat image = cv::imread("./Tux.png");
+
+  if (!image.data)
+  {
+    printf("No image data \n");
+    return -1;
+  }
+  cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Display Image", image);
+
+  cv::waitKey(0);
+
+  return 0;
+}
