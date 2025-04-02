20d09e1fb32d307e558a9aaeade5d2f4506a49b4
Alan Manuel Loreto Cornidez <aloretocornidez@gmail.com>
2025-04-01 22:10:09 -0700

    updates to cmake

--- cmake-workshop/example4-using-cmake/main.cpp
+++ cmake-workshop/example4-using-cmake/main.cpp
@@ -1,19 +1,34 @@
-#include <opencv2/opencv.hpp>
+// #include "opencv2/highgui.hpp"
+// #include <opencv2/opencv.hpp>
+// #include <iostream>
+// #include <string>
 
 int main(int argc, char *argv[])
 {
 
   // Print out hello world.
   // std::cout << "Hello World" << std::endl;
+  std::string imageName;
 
-  cv::Mat image = cv::imread(argv[1]);
+  if (argc > 1)
+  {
+    imageName = argv[1];
+  }
+  else
+  {
+    imageName = "/home/alan-manuel/Pictures/IMG_2969.jpg";
+
+    std::cout << "No image path specified, using: " << imageName << std::endl;
+  };
+
+  cv::Mat image = cv::imread(imageName);
 
   if (!image.data)
   {
     printf("No image data \n");
     return -1;
   }
-  cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("Display Image", cv::WINDOW_NORMAL);
   cv::imshow("Display Image", image);
 
   cv::waitKey(0);
